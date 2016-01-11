6b5ccb002420829e4929b876173de826f48ad67d
vtechmonkey <sarah.oconnor@webinnovate.ie>
2016-01-11 15:28:24 +0000

    more tweaks to nav menu and blog page in large screen view add 'lightbox' to enlarge images

--- GuardianBootstrapHtml/js/guardian.js
+++ GuardianBootstrapHtml/js/guardian.js
@@ -1,12 +1,23 @@
-
-
-$(document).ready(function(){
-    $('#laterTag').hover(function() {
-        $('#later').addClass('laterIconHover');
+$(document).ready(function () {
+  $('#laterTag').hover(function () {
+      $('#later').addClass('laterIconHover');
     },
-           function(){
-               $('#later').removeClass('laterIconHover');
-        }
-    );
+    function () {
+      $('#later').removeClass('laterIconHover');
+    }
+  );
 });
+
+//$(document).ready(function () {
+//  $('.blogImgSize').css({ "max-width": "100%" });
+//})
+
+//$('.glyphicon-resize-full').click(function () {
+//    $(console.log("boo"));
+//  $('.blogImgSize').dialog();
+   // $('.blogImgSize').toggleClass( "blogImgLrg");
+
+  //})
+
+
 
